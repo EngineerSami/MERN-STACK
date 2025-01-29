27dbf5f3d505dde76c21bccb69aebd49de63080a
Sami <sami.daraghmeh1306@gmail.com>
2025-01-30 00:29:27 +0200

    sdsa

--- mern-project/backend/server.js
+++ mern-project/backend/server.js
@@ -3,7 +3,7 @@ require('./config/mongoose.config');
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 app.use(cors());
